6651b636d0c3f5af3c97efbcf94019e8ec6a2adc
root <root@DESKTOP-QVABO1H.localdomain>
2021-10-31 13:18:01 +0700

    fresh

---
+++ sesi6/source1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void cetakUmur(int *umur){
+ printf("Halo, Umur kamu adalah %d tahun", *umur);
+}
+int main(){
+ int angka=20;
+ cetakUmur(&angka);
+ getchar();
+}
